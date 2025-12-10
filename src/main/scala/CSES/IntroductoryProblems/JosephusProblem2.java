303c418d0510debfd1c10df248701932b7b59a0f
David Paneque <davidpaneque@MacBook-Pro-David.local>
2025-12-10 10:21:31 +0300

    josephus 2

--- src/main/scala/CSES/IntroductoryProblems/JosephusProblem2.java
+++ src/main/scala/CSES/IntroductoryProblems/JosephusProblem2.java
@@ -3,19 +3,16 @@ package CSES.IntroductoryProblems;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.TreeSet;
 
 class SegTree {
-    //int n;
     ArrayList<Integer> tree;
 
     public SegTree(int n) {
-        //this.n = n;
         this.tree = new ArrayList<>(4 * n);
         for (int i = 0; i < 4 * n; i++) {
             this.tree.add(i, 0);
         }
-        build(1, 0, n - 1);
+        build(1, 1, n );
     }
 
     public void build(int v, int l, int r) {
@@ -27,8 +24,19 @@ class SegTree {
         }
     }
 
+    public int calcAlive(int to, int v, int l, int r) {
+        if (to == 0) return 0;
+        if (to == r) return tree.get(v);
+        int mid = (l + r) / 2;
+        if (to <= mid) {
+            return calcAlive(to,2 * v, l, mid);
+        } else {
+            return tree.get(2 * v) + calcAlive(to,2 * v + 1, mid + 1, r);
+        }
+    }
+
     public int posOfKnonZero(int k, int v, int l, int r) {
-        if (tree.get(v) == k) return r;
+        if (l == r && k == 1) return r;
         int mid = (l + r) / 2;
         if (k <= tree.get(2 * v)) {
             return posOfKnonZero(k, 2 * v, l, mid);
@@ -54,7 +62,7 @@ class SegTree {
 public class JosephusProblem2 {
     /**
      * O(n log n)
-     * Просто использовать TreeSet
+     * Просто использовать SegTree
      */
     static PrintWriter pw = new PrintWriter(System.out);
     public static void main(String[] args) {
@@ -62,20 +70,15 @@ public class JosephusProblem2 {
         int n = sc.nextInt();
         int k = sc.nextInt();
 
-        int curPos = 0;
-        int nextPos = 0;
+        int pos = 0;
         SegTree tree = new SegTree(n);
         for (int i = 0; i < n; i++) {
-            nextPos = (curPos + k + 1) % tree.getNumAlive();
-            pw.print(nextPos);
+            int numOnes = (tree.calcAlive(pos,1, 1, n) + k + 1) % tree.getNumAlive();
+            if (numOnes == 0) numOnes = tree.getNumAlive();
+            pos = tree.posOfKnonZero(numOnes, 1, 1, n);
+            pw.print(pos);
             pw.print(' ');
-            pw.flush();
-            tree.fillZero(nextPos, 1, 0, n - 1);
-
-            nextPos = tree.posOfKnonZero(, 1, 0, n - 1);
-
-
-            //nextPos = (nextPos + k + 1) % tree.getNumAlive();
+            tree.fillZero(pos, 1, 1, n);
         }
         pw.flush();
     }
